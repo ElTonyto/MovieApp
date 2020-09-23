a26db13a3e6d5b76e3fed558856fc610ed83c5e1
Tony Pedrero <tony.pedrero@ynov.com>
2020-09-23 16:55:52 +0200

    put working

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ app.get('/users', async (req, res) => {
   //Select all customers and return the result object:
   response = con.query("SELECT * FROM users", function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     res.send(JSON.stringify(result));
+    console.log("Get All users");
   });
 
 });
@@ -54,47 +54,55 @@ app.get('/users/:id', async (req, res) => {
 
   response = con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     res.send(JSON.stringify(result));
+    console.log(`Get user ${req.params.id}`);
   });
 });
-
-/*
-app.get('/users?limit=20&offset=0', async (req, res) => {
-
-});
-*/
 
 app.delete('/users/:id', async (req, res) => {
   var sql = `DELETE FROM users WHERE id = ${req.params.id}`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(JSON.stringify(result));
-    console.log("Number of records deleted: " + result.affectedRows);
+    console.log(`Deleted user ${req.params.id}`);
+
   });
 });
 
 app.put('/users/:id', async (req, res) => {
 
+  let user;
+  let select = `SELECT * FROM users
+  WHERE users.id = ${req.params.id};`;
 
-
-  let sql = `UPDATE users SET VALUES 
-    (NULL,
-      ${con.escape(req.body.pseudo)},
-      ${con.escape(req.body.email)},
-      ${con.escape(req.body.firstname)},
-      ${con.escape(req.body.lastname)},
-      ${con.escape(new Date().toLocaleString())},
-      ${con.escape(new Date().toLocaleString())}, 
-      WHERE users.id = ${req.params.id})`;
-
-  con.query(sql, function (err, result) {
+  con.query(select, function (err, result, fields) {
     if (err) throw err;
-    res.send(JSON.stringify(result));
-    console.log("1 record inserted");
+    console.log(result);
+    user = result[0];
+    console.log(user);
+
+    if (req.body.pseudo) user.pseudo = req.body.pseudo;
+    console.log(req.body);
+    if (req.body.email) user.email = req.body.email;
+    if (req.body.firstname) user.firstname = req.body.firstname;
+    if (req.body.lastname) user.lastname = req.body.lastname;
+
+    let sql = `UPDATE users SET 
+      pseudo = ${con.escape(user.pseudo)},
+      email = ${con.escape(user.email)},
+      firstname = ${con.escape(user.firstname)},
+      lastname = ${con.escape(user.lastname)},
+      upadatedat = ${con.escape(new Date().toLocaleString())} 
+      WHERE id = ${req.params.id}`;
+
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      res.send(JSON.stringify(result));
+      console.log(`Upadated user ${req.params.id}`);
+    });
   });
 
-});
 
+});
 
 app.listen(3030);
